994d616636688c67221c559d0013c184d1da3714
linkdrone <fengwanqing2014@gmail.com>
2023-02-28 14:40:50 +0800

    updates

--- hardhat/test/ECC.test.ts
+++ hardhat/test/ECC.test.ts
@@ -1,12 +1,29 @@
 
+
 describe("ECC", function () {
   this.timeout(2000000);
 
+  before(async function () {});
 
-  before(async function () {
-  });
+  it("Test mod and bit", async () => {
+    const preTime = new Date().getTime();
 
-  it("ecc", async () => {
-    
+    let a = 0;
+    for (let i = 0; i < 999999909; i++) {
+      if (i % 2) {
+        a = 1;
+      }
+    }
+    console.warn("a:", a, ", time:", new Date().getTime() - preTime);
+
+    const preTime2 = new Date().getTime();
+
+    let b = 0;
+    for (let i = 0; i < 999999909; i++) {
+      if (i & 1) {
+        b = 1;
+      }
+    }
+    console.warn("b:", b, ", time:", new Date().getTime() - preTime2);
   });
 });
