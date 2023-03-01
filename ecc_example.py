89e325fecf93875e6b7aca9a872300a3f8906518
linkdrone <fengwanqing2014@gmail.com>
2023-03-01 18:36:51 +0800

    updates

---
+++ ecc_example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*-
+
+# 
+# 1. python3 -m venv ./python_venv
+# 2. source ./python_venv/bin/activate
+# 3. python3 -m pip install --upgrade pip
+# 4. pip3 install -r requirements.txt
+# 5. python3 ecc_example.py
+# 
